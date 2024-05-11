35a114e395dfe97434b9d5b0e2d39f3364faaf40
MrXck <1971877559@qq.com>
2024-05-11 11:09:40 +0800

    修复邮箱验证时邮箱为null会报错这个问题

--- src/main/java/com/todo/utils/EmailValidatorUtils.java
+++ src/main/java/com/todo/utils/EmailValidatorUtils.java
@@ -9,6 +9,9 @@ public class EmailValidatorUtils {
     private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
     public static boolean isValid(String email) {
+        if (email == null) {
+            return false;
+        }
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
